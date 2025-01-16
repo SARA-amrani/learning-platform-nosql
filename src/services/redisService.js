0b1e57dabe2aef4dcfa377bd57c211b408d6512c
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-01-16 11:13:28 +0100

    update redisService

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -5,12 +5,45 @@
 
 
 const { getRedisClient } = require("../config/db");
+const redisClient = require('../config/db').redisClient;
+
 
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
-    // TODO: Implémenter une fonction générique de cache
+    //Implémenter d'une fonction générique de cache
+    const redisClient = getRedisClient();
+  
+  if (!redisClient) {
+    console.error("Redis client non initialise.!!!");
+    throw new Error("Redis client non initialise!!!.");
   }
+
+  try {
+    await redisClient.setEx(key, ttl, JSON.stringify(data));
+    console.log(`Donnees mises en cache est reussie sous la cle ${key}`);
+  } catch (error) {
+    console.error("!!!Erreur lors du stockage dans le cache Redis:", error);
+    throw new Error("Erreur de mise en cache dans Redis!!!");
+  }
+  }
+
+  //fonction pour recuperer  les donnees du cache
+async function getCacheData(key) {
+  try {
+    const cachedData = await redisClient.get(key);
+
+    if (cachedData) {
+      return JSON.parse(cachedData);
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(`Erreur lors de la recuperation du cache Redis : ${error.message}`);
+    throw new Error('Erreur de recuperation du cache Redis!!!');
+  }
+}
   
   module.exports = {
-    // TODO: Exporter les fonctions utilitaires
+    cacheData,
+    getCacheData,
   };
